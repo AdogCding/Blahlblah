b9162a7409c8b84888634a85d53e74876d80644d
Keyang Li <liky1995@hotmail.com>
2023-04-23 22:14:51 +0800

    支持katex

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,15 @@
 module.exports = {
-    title: 'AcodingDog的博客',
-    description: "AcodingDog's Blog"
-  }
+  title: "AcodingDog的博客",
+  description: "AcodingDog's Blog",
+  plugins: {
+    "@maginapp/katex": {
+      delimiters: "dollars",
+    },
+  },
+  // or
+  plugins: {
+    "@maginapp/vuepress-plugin-katex": {
+      delimiters: "dollars",
+    },
+  },
+};
