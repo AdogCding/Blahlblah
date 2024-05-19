185fe9d5566dae79791c919362fa14fc2ffc3d53
Keyang Li <liky1995@hotmail.com>
2024-05-19 19:58:42 +0800

    init deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+if [-d ~/dist];then
+    echo "remove deprecated dist" && rm -rf ~/dist
+fi
+if [! -d docs/.vuepress/dist/];then
+    echo "run build cmd first!"
+    exit 1
+fi
+cp -r docs/.vuepress/dist/ ~/dist/
