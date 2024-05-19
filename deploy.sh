8cf15fda8ef10bc47a760fa0f7e45c532fb1f78d
Keyang Li <liky1995@hotmail.com>
2024-05-19 20:07:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-if [-d ~/dist];then
+if [ -d ~/dist];then
     echo "remove deprecated dist" && rm -rf ~/dist
 fi
-if [! -d docs/.vuepress/dist/];then
+if [ ! -d docs/.vuepress/dist/];then
     echo "run build cmd first!"
     exit 1
 fi
